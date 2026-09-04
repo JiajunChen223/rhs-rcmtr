e57055895efc23a8c33d42b4b0cee468f789b96c
JiajunChen223 <chenjiajun232@gmail.com>
2026-09-04 09:00:45 +0800

    R7 S2FT: export predeclared variants

--- src/rhs_rcmtr/models/s2ft/__init__.py
+++ src/rhs_rcmtr/models/s2ft/__init__.py
@@ -1,16 +1,18 @@
 """R7 Sensor-Symmetric Foundation Transfer architecture."""
 
-from .config import S2FTConfig
+from .config import R7_S2FT_VARIANTS, S2FTConfig, s2ft_config_for_variant
 from .model import S2FTSegmenter
 from .patch_transplant import SarFoundationPatchEmbed
 from .sensor_adapter import SensorAdapter, SensorAdapterPair
 from .shared_foundation import SharedDinoSensorBackbone
 
 __all__ = [
+    "R7_S2FT_VARIANTS",
     "S2FTConfig",
     "S2FTSegmenter",
     "SarFoundationPatchEmbed",
     "SensorAdapter",
     "SensorAdapterPair",
     "SharedDinoSensorBackbone",
+    "s2ft_config_for_variant",
 ]
